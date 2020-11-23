c05cb7c1615b370503fd1e229abd6b54c23f67bb
Adam Sanderson <adam@adamsanderson.co.uk>
2020-11-23 18:08:34 +0000

    add ga?

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,26 @@
   export let name;
   import GoogleAnalytics from "./GoogleAnalytics.svelte";
 
+  (function (i, s, o, g, r, a, m) {
+    i["GoogleAnalyticsObject"] = r;
+    (i[r] =
+      i[r] ||
+      function () {
+        (i[r].q = i[r].q || []).push(arguments);
+      }),
+      (i[r].l = 1 * new Date());
+    (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m);
+  })(
+    window,
+    document,
+    "script",
+    "https://www.google-analytics.com/analytics.js",
+    "ga"
+  );
+
   function buttonHandler() {
     ga("send", "event", "Button", "test", "Test button hit");
   }
